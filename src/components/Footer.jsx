f1c8487d7f0cd8216ef17b4ad93aaf7fd7523116
parthosarothi46 <parthosarothi46@gmail.com>
2024-12-08 02:09:18 +0600

    footer completed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,13 +3,13 @@ import { Link } from "react-router";
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-900 pt-12 pb-6 px-10 font-[sans-serif] tracking-wide">
+    <footer className="bg-white pt-12 pb-6 px-10 tracking-wide">
       <div className="max-w-screen-xl mx-auto">
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
           {/* Logo Section */}
           <div className="lg:flex lg:items-center">
             <Link to="/">
-              <p className="text-2xl text-gray-300 font-bold">EquiSports</p>
+              <p className="text-2xl text-black font-bold">EquiSports</p>
             </Link>
           </div>
 
@@ -20,7 +20,7 @@ const Footer = () => {
                 <a href="#">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
-                    className="fill-gray-300 hover:fill-white w-7 h-7"
+                    className="fill-black hover:fill-black w-7 h-7"
                     viewBox="0 0 24 24"
                   >
                     <path
@@ -35,7 +35,7 @@ const Footer = () => {
                 <a href="#">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
-                    className="fill-gray-300 hover:fill-white w-7 h-7"
+                    className="fill-black hover:fill-black w-7 h-7"
                     viewBox="0 0 24 24"
                   >
                     <path
@@ -51,7 +51,7 @@ const Footer = () => {
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
-                    className="fill-gray-300 hover:fill-white w-7 h-7"
+                    className="fill-black hover:fill-black w-7 h-7"
                     viewBox="0 0 24 24"
                   >
                     <path d="M22.92 6c-.77.35-1.6.58-2.46.69.88-.53 1.56-1.37 1.88-2.38-.83.5-1.75.85-2.72 1.05C18.83 4.5 17.72 4 16.46 4c-2.35 0-4.27 1.92-4.27 4.29 0 .34.04.67.11.98-3.56-.18-6.73-1.89-8.84-4.48-.37.63-.58 1.37-.58 2.15 0 1.49.75 2.81 1.91 3.56-.71 0-1.37-.2-1.95-.5v.03c0 2.08 1.48 3.82 3.44 4.21a4.22 4.22 0 0 1-1.93.07 4.28 4.28 0 0 0 4 2.98 8.521 8.521 0 0 1-5.33 1.84c-.34 0-.68-.02-1.02-.06C3.9 20.29 6.16 21 8.58 21c7.88 0 12.21-6.54 12.21-12.21 0-.19 0-.37-.01-.56.84-.6 1.56-1.36 2.14-2.23z" />
@@ -66,12 +66,12 @@ const Footer = () => {
             <h4 className="text-lg mb-6 text-white">Useful Links</h4>
             <ul className="space-y-4 pl-2">
               <li>
-                <a href="#" className="text-gray-300 hover:text-white text-sm">
+                <a href="#" className="text-black hover:text-black text-sm">
                   Featured
                 </a>
               </li>
               <li>
-                <a href="#" className="text-gray-300 hover:text-white text-sm">
+                <a href="#" className="text-black hover:text-black text-sm">
                   New Arrivals
                 </a>
               </li>
@@ -80,27 +80,27 @@ const Footer = () => {
 
           {/* Information Links */}
           <div>
-            <h4 className="text-lg mb-6 text-white">Information</h4>
+            <h4 className="text-lg mb-6 text-black">Information</h4>
             <ul className="space-y-4 pl-2">
               <li>
-                <a href="#" className="text-gray-300 hover:text-white text-sm">
+                <a href="#" className="text-black hover:text-black text-sm">
                   About Us
                 </a>
               </li>
               <li>
-                <a href="#" className="text-gray-300 hover:text-white text-sm">
+                <a href="#" className="text-black hover:text-black text-sm">
                   Terms & Conditions
                 </a>
               </li>
               <li>
-                <a href="#" className="text-gray-300 hover:text-white text-sm">
+                <a href="#" className="text-black hover:text-black text-sm">
                   Privacy Policy
                 </a>
               </li>
             </ul>
           </div>
         </div>
-        <p className="text-gray-300 text-sm mt-10">
+        <p className="text-black text-sm mt-10">
           © EquiSports. All rights reserved.
         </p>
       </div>
