17e38d7973c6c7aa951eac73eeaf94d6f6d3f4c6
parthosarothi46 <parthosarothi46@gmail.com>
2024-12-08 16:09:14 +0600

    register dark mode completed

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,3 +1,173 @@
+// import { useState } from "react";
+// import { useAuth } from "../context/AuthContext"; // Auth context for Firebase actions
+// import { Link, useNavigate } from "react-router-dom";
+// import { toast } from "react-toastify";
+// import "react-toastify/dist/ReactToastify.css";
+// import { updateProfile } from "firebase/auth";
+
+// const RegisterPage = () => {
+//   const [name, setName] = useState("");
+//   const [email, setEmail] = useState("");
+//   const [photoURL, setPhotoURL] = useState("");
+//   const [password, setPassword] = useState("");
+//   const [loading, setLoading] = useState(false);
+//   const { register } = useAuth(); // Auth methods
+//   const navigate = useNavigate();
+
+//   const validatePassword = (password) => {
+//     return /^(?=.*[a-z])(?=.*[A-Z]).{6,}$/.test(password);
+//   };
+
+//   const handleRegister = async (e) => {
+//     e.preventDefault();
+
+//     if (!validatePassword(password)) {
+//       toast.error(
+//         "Password must contain at least 6 characters, including uppercase and lowercase letters."
+//       );
+//       return;
+//     }
+
+//     setLoading(true);
+
+//     try {
+//       const userCredential = await register(email, password);
+//       await updateProfile(userCredential.user, { displayName: name, photoURL });
+//       toast.success("Registration successful!");
+//       navigate("/");
+//     } catch (error) {
+//       console.error("Registration Error: ", error);
+//       toast.error(error.message || "Registration failed. Please try again.");
+//     } finally {
+//       setLoading(false);
+//     }
+//   };
+
+//   return (
+//     <div className=" bg-white m-5">
+//       <div className="grid md:grid-cols-2 items-center gap-8 h-full">
+//         <div className="max-md:order-1 p-4 bg-gray-50 h-full">
+//           <img
+//             src="https://readymadeui.com/signin-image.webp"
+//             className="lg:max-w-[90%] w-full h-full object-contain block mx-auto"
+//             alt="login-image"
+//           />
+//         </div>
+//         <div className="flex items-center p-6 h-full w-full">
+//           <form className="max-w-lg w-full mx-auto" onSubmit={handleRegister}>
+//             <div className="mb-12">
+//               <h3 className="text-black md:text-3xl text-2xl font-extrabold max-md:text-center">
+//                 Create an account
+//               </h3>
+//             </div>
+//             <div>
+//               <label className="text-gray-800 text-xs block mb-2">
+//                 Full Name
+//               </label>
+//               <div className="relative flex items-center">
+//                 <input
+//                   name="name"
+//                   type="text"
+//                   value={name}
+//                   onChange={(e) => setName(e.target.value)}
+//                   required
+//                   className="w-full bg-transparent text-sm border-b border-gray-300 focus:border-black px-2 py-3 outline-none"
+//                   placeholder="Enter name"
+//                 />
+//               </div>
+//             </div>
+//             <div className="mt-6">
+//               <label className="text-gray-800 text-xs block mb-2">Email</label>
+//               <div className="relative flex items-center">
+//                 <input
+//                   name="email"
+//                   type="email"
+//                   value={email}
+//                   onChange={(e) => setEmail(e.target.value)}
+//                   required
+//                   className="w-full bg-transparent text-sm border-b border-gray-300 focus:border-black px-2 py-3 outline-none"
+//                   placeholder="Enter email"
+//                 />
+//               </div>
+//             </div>
+//             <div className="mt-6">
+//               <label className="text-gray-800 text-xs block mb-2">
+//                 Photo URL
+//               </label>
+//               <div className="relative flex items-center">
+//                 <input
+//                   name="photoURL"
+//                   type="url"
+//                   value={photoURL}
+//                   onChange={(e) => setPhotoURL(e.target.value)}
+//                   className="w-full bg-transparent text-sm border-b border-gray-300 focus:border-black px-2 py-3 outline-none"
+//                   placeholder="Enter photo URL (optional)"
+//                 />
+//               </div>
+//             </div>
+//             <div className="mt-6">
+//               <label className="text-gray-800 text-xs block mb-2">
+//                 Password
+//               </label>
+//               <div className="relative flex items-center">
+//                 <input
+//                   name="password"
+//                   type="password"
+//                   value={password}
+//                   onChange={(e) => setPassword(e.target.value)}
+//                   required
+//                   className="w-full bg-transparent text-sm border-b border-gray-300 focus:border-black px-2 py-3 outline-none"
+//                   placeholder="Enter password"
+//                 />
+//               </div>
+//             </div>
+//             <div className="flex items-center mt-6">
+//               <input
+//                 id="terms"
+//                 name="terms"
+//                 type="checkbox"
+//                 required
+//                 className="h-4 w-4 shrink-0 rounded"
+//               />
+//               <label
+//                 htmlFor="terms"
+//                 className="ml-3 block text-sm text-gray-800"
+//               >
+//                 I accept the{" "}
+//                 <a
+//                   href="/terms"
+//                   className="text-black font-semibold hover:underline ml-1"
+//                 >
+//                   Terms and Conditions
+//                 </a>
+//               </label>
+//             </div>
+//             <div className="mt-12">
+//               <button
+//                 type="submit"
+//                 className="w-full py-3 px-6 text-sm tracking-wider font-semibold rounded-md bg-black hover:bg-black text-white focus:outline-none"
+//                 disabled={loading}
+//               >
+//                 {loading ? "Registering..." : "Create an account"}
+//               </button>
+//               <p className="text-sm mt-6 text-gray-800">
+//                 Already have an account?{" "}
+//                 <Link to="/login">
+//                   <a className="text-black font-semibold hover:underline ml-1">
+//                     Login here
+//                   </a>
+//                 </Link>
+//               </p>
+//             </div>
+//           </form>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default RegisterPage;
+
 import { useState } from "react";
 import { useAuth } from "../context/AuthContext"; // Auth context for Firebase actions
 import { Link, useNavigate } from "react-router-dom";
@@ -44,9 +214,9 @@ const RegisterPage = () => {
   };
 
   return (
-    <div className=" bg-white m-5">
+    <div className="bg-white dark:bg-gray-900 py-5">
       <div className="grid md:grid-cols-2 items-center gap-8 h-full">
-        <div className="max-md:order-1 p-4 bg-gray-50 h-full">
+        <div className="max-md:order-1 p-4 bg-gray-50 dark:bg-gray-800 h-full">
           <img
             src="https://readymadeui.com/signin-image.webp"
             className="lg:max-w-[90%] w-full h-full object-contain block mx-auto"
@@ -56,12 +226,12 @@ const RegisterPage = () => {
         <div className="flex items-center p-6 h-full w-full">
           <form className="max-w-lg w-full mx-auto" onSubmit={handleRegister}>
             <div className="mb-12">
-              <h3 className="text-black md:text-3xl text-2xl font-extrabold max-md:text-center">
+              <h3 className="text-black dark:text-white md:text-3xl text-2xl font-extrabold max-md:text-center">
                 Create an account
               </h3>
             </div>
             <div>
-              <label className="text-gray-800 text-xs block mb-2">
+              <label className="text-gray-800 dark:text-gray-200 text-xs block mb-2">
                 Full Name
               </label>
               <div className="relative flex items-center">
@@ -71,13 +241,15 @@ const RegisterPage = () => {
                   value={name}
                   onChange={(e) => setName(e.target.value)}
                   required
-                  className="w-full bg-transparent text-sm border-b border-gray-300 focus:border-black px-2 py-3 outline-none"
+                  className="w-full bg-transparent text-sm border-b border-gray-300 focus:border-black dark:border-gray-600 focus:ring-0 px-2 py-3 outline-none"
                   placeholder="Enter name"
                 />
               </div>
             </div>
             <div className="mt-6">
-              <label className="text-gray-800 text-xs block mb-2">Email</label>
+              <label className="text-gray-800 dark:text-gray-200 text-xs block mb-2">
+                Email
+              </label>
               <div className="relative flex items-center">
                 <input
                   name="email"
@@ -85,13 +257,13 @@ const RegisterPage = () => {
                   value={email}
                   onChange={(e) => setEmail(e.target.value)}
                   required
-                  className="w-full bg-transparent text-sm border-b border-gray-300 focus:border-black px-2 py-3 outline-none"
+                  className="w-full bg-transparent text-sm border-b border-gray-300 focus:border-black dark:border-gray-600 focus:ring-0 px-2 py-3 outline-none"
                   placeholder="Enter email"
                 />
               </div>
             </div>
             <div className="mt-6">
-              <label className="text-gray-800 text-xs block mb-2">
+              <label className="text-gray-800 dark:text-gray-200 text-xs block mb-2">
                 Photo URL
               </label>
               <div className="relative flex items-center">
@@ -100,13 +272,13 @@ const RegisterPage = () => {
                   type="url"
                   value={photoURL}
                   onChange={(e) => setPhotoURL(e.target.value)}
-                  className="w-full bg-transparent text-sm border-b border-gray-300 focus:border-black px-2 py-3 outline-none"
+                  className="w-full bg-transparent text-sm border-b border-gray-300 focus:border-black dark:border-gray-600 focus:ring-0 px-2 py-3 outline-none"
                   placeholder="Enter photo URL (optional)"
                 />
               </div>
             </div>
             <div className="mt-6">
-              <label className="text-gray-800 text-xs block mb-2">
+              <label className="text-gray-800 dark:text-gray-200 text-xs block mb-2">
                 Password
               </label>
               <div className="relative flex items-center">
@@ -116,7 +288,7 @@ const RegisterPage = () => {
                   value={password}
                   onChange={(e) => setPassword(e.target.value)}
                   required
-                  className="w-full bg-transparent text-sm border-b border-gray-300 focus:border-black px-2 py-3 outline-none"
+                  className="w-full bg-transparent text-sm border-b border-gray-300 focus:border-black dark:border-gray-600 focus:ring-0 px-2 py-3 outline-none"
                   placeholder="Enter password"
                 />
               </div>
@@ -131,12 +303,12 @@ const RegisterPage = () => {
               />
               <label
                 htmlFor="terms"
-                className="ml-3 block text-sm text-gray-800"
+                className="ml-3 block text-sm text-gray-800 dark:text-gray-200"
               >
                 I accept the{" "}
                 <a
                   href="/terms"
-                  className="text-black font-semibold hover:underline ml-1"
+                  className="text-black dark:text-white font-semibold hover:underline ml-1"
                 >
                   Terms and Conditions
                 </a>
@@ -150,10 +322,10 @@ const RegisterPage = () => {
               >
                 {loading ? "Registering..." : "Create an account"}
               </button>
-              <p className="text-sm mt-6 text-gray-800">
+              <p className="text-sm mt-6 text-gray-800 dark:text-gray-200">
                 Already have an account?{" "}
                 <Link to="/login">
-                  <a className="text-black font-semibold hover:underline ml-1">
+                  <a className="text-black dark:text-white font-semibold hover:underline ml-1">
                     Login here
                   </a>
                 </Link>
